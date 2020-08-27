95ab247ea33027894877e115226af0113d576d89
vicky <jiajun_work@126.com>
2020-08-27 14:48:24 +0800

    [refactor exercise][vicky] test: add should return owed 1730 and 47 earned when statement given all performances.

--- test/statementTest.js
+++ test/statementTest.js
@@ -126,3 +126,32 @@ const invoice = {
     'Amount owed is $468.00\n' +
     'You earned 4 credits \n');
 })
+test('should return owed 1730 and 47 earned when statement given all performances',t => {
+//given
+const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+//when
+  const result = statement(invoice, plays);
+//then
+  t.is(result, 'Statement for BigCo\n' +
+    ' Hamlet: $650.00 (55 seats)\n' +
+    ' As You Like It: $580.00 (35 seats)\n' +
+    ' Othello: $500.00 (40 seats)\n' +
+    'Amount owed is $1,730.00\n' +
+    'You earned 47 credits \n');
+})
