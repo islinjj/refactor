72e9568287a4437351afa32b268ee0a4674abda8
vicky <jiajun_work@126.com>
2020-08-27 14:40:44 +0800

    [refactor exercise][vicky] test: add should return 0 earned and 650 owed when statement given hamlet and 30 audience.

--- test/statementTest.js
+++ test/statementTest.js
@@ -35,7 +35,7 @@ const plays = {
   },
 };
 
-test('statement case 1, Customer Bigco without performance', t => {
+test('should return 0 earn and 0 owed when statement given no performances', t => {
   //given
   const invoice = {
     'customer': 'BigCo',
@@ -49,3 +49,23 @@ test('statement case 1, Customer Bigco without performance', t => {
   //then
   t.is(result,expectResult);
 });
+
+test('should return 0 earned and 650 owed when statement given hamlet and 30 audience', t => {
+  //given
+  const invoice = {
+      'customer': 'BigCo',
+      'performances': [
+        {
+          'playID': 'hamlet',
+          'audience': 30,
+        },
+      ],
+    };
+  //when
+   const result = statement(invoice, plays);
+  //then
+   t.is(result, 'Statement for BigCo\n' +
+      ' Hamlet: $400.00 (30 seats)\n' +
+      'Amount owed is $400.00\n' +
+      'You earned 0 credits \n');
+})
